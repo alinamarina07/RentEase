2849b39438734e29ff3761f70e81431194aadb34
alinamarina07 <137393068+alinamarina07@users.noreply.github.com>
2024-07-12 19:14:53 +0300

    commit

---
+++ js/newFlat.js
@@ -0,0 +1,12 @@
+const newFlatForm = document.getElementById('newFlatForm');
+const name = document.getElementById('name');
+const city = document.getElementById('city');
+const streetName = document.getElementById('streetName');
+const streetNumber = document.getElementById('streetNumber');
+const areaSize = document.getElementById('areaSize');
+const hasAC = document.getElementById('hasAC');
+const yearBuilt = document.getElementById('yearBuilt');
+const rentPrice = document.getElementById('rentPrice');
+const dateAvailable = document.getElementById('dateAvailable');
+const saveBtn = document.getElementById('saveBtn');
+
